779b258d29e3e9cb85707c37781240bfb22e0d3c
Keisean5 <seanksarreal@gmail.com>
2025-08-12 14:37:00 -0700

    Implemented useEffect and Imported Font Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import TaskForm from "./components/TaskForm";
 import TaskColumn from "./components/TaskColumn";
 import todoIcon from "./assets/direct-hit.png";
 import doingIcon from "./assets/glowing-star.png";
 import doneIcon from "./assets/check-mark-button.png";
-import { useState } from "react";
+
+const oldTasks = localStorage.getItem("tasks");
+console.log(oldTasks);
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks)); //stringify will convert the tasks array to a string. localStorage only takes strings
+  }, [tasks]);
 
   const handleDelete = (taskIndex) => {
     const newTasks = tasks.filter((task, index) => index !== taskIndex);
