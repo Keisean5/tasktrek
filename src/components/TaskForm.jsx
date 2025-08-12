15b9fec2920908c51eae31a20ebe80a53a4962ac
Keisean5 <seanksarreal@gmail.com>
2025-08-11 17:28:54 -0700

    Added Form Handling for Task Input

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+  //Create Single State variable, which is an object
+  const [taskData, setTaskData] = useState({
+    //Property Names and their Values
+    task: "",
+    status: "todo",
+  });
+
+  //one single function for all form Fields on change event
+  const handleChange = (e) => {
+    //get the name and value attribute from event object.
+    const { name, value } = e.target;
+    //replace the current value of our task data object
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
+
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
-          type="text"
+          //pass the name properties to match the object properties
+          type="text" // exactly the same in taskData Oject
+          name="task" // exactly the same in taskData Oject
           className="task_input"
           placeholder="Enter your task"
+          onChange={handleChange}
         />
         <div className="task_form_bottom_line">
           <div>
@@ -21,7 +46,11 @@ const TaskForm = () => {
           </div>
 
           <div>
-            <select className="task_status">
+            <select
+              name="status"
+              className="task_status"
+              onChange={handleChange}
+            >
               <option value="todo">To do</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
